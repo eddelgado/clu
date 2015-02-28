c8ee8d44a65eda310bc8ec1bd5339a139c0a95ce
Benjamin Hudgens <benjamin@benjamindavid.com>
2015-02-28 00:36:40 -0600

    Change encoding of readFile

--- scripts/starphleet.js
+++ scripts/starphleet.js
@@ -99,7 +99,7 @@ module.exports = function(robot) {
     console.log("P: " + _path);
     // The status files in starphleet contain a small blurb about
     // the deployment status of this service.  Just return it to the user
-    fs.readFile(_path, function(err, data) {
+    fs.readFile(_path, 'utf8', function(err, data) {
       // Throw an exception if we have a problem
       if (err) {
         throw "File Error";
