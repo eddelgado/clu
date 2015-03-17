f256af1bafea7a7773d90f2e20e2f753203b5424
Benjamin Hudgens <benjamin@benjamindavid.com>
2015-03-17 13:21:37 -0500

    LDAP URL's maybe

---
+++ scripts/ldap.js
@@ -0,0 +1,16 @@
+// Description
+//   Make an epildap link
+//
+// Commands:
+//   ldap <search>
+
+var epiLdapUrl = "https://jobs.glgresearch.com/epildap/searchldap?cn=";
+
+module.exports = function(robot) {
+
+  robot.hear(/(^ldap)\s+(.*)/i, function(msg) {
+    var _search = msg.match[2];
+    msg.send(epiLdapUrl + _search + "*");
+  });
+
+};
