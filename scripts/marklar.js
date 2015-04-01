94e8c55d6977a78fe0e268b3f44affcd2cf86bc3
Benjamin Hudgens <benjamin@benjamindavid.com>
2015-04-01 18:05:45 -0500

    Add Marklar details url

---
+++ scripts/marklar.js
@@ -0,0 +1,17 @@
+// Description
+//   Make an epildap link
+//
+// Commands:
+//   ldap <search>
+
+var consultationUrl = "services.glgresearch.com/marklar/api/call/details?consultationId=";
+
+module.exports = function(robot) {
+
+  robot.hear(/.*consultations\/#\/consultation\/(\d+)$/, function(msg) {
+  robot.hear(/(^ldap)\s+(.*)/i, function(msg) {
+    var _consultationId = msg.match[1];
+    msg.send(consultationUrl + encodeURIComponent(_consultationId));
+  });
+
+}consulta
