508f6ad88f09a6268aafb8f2639e1ca3000c00f9
Alexis Jackson <amjackson09@gmail.com>
2016-06-17 12:34:10 -0600

    search by username

--- scripts/ldap.js
+++ scripts/ldap.js
@@ -3,14 +3,27 @@
 //
 // Commands:
 //   ldap <search>
+//   ldapuser <search>
 
-var epiLdapUrl = "https://services.glgresearch.com/epildap/searchldap?cn=";
+var epiLdapUrl = "";
+
+function getURL(type) {
+  epiLdapUrl = "https://services.glgresearch.com/epildap/searchldap?" + type + "=";
+  return epiLdapUrl;
+}
 
 module.exports = function(robot) {
 
   robot.hear(/(^ldap)\s+(.*)/i, function(msg) {
     var _search = msg.match[2];
+    getURL("cn");
     msg.send(epiLdapUrl + encodeURIComponent(_search) + "*");
   });
-
+  
+  robot.hear(/(^ldapuser)\s+(.*)/i, function(msg) {
+    var _search = msg.match[2];
+    getURL("sAMAccountName");
+    msg.send(epiLdapUrl + encodeURIComponent(_search) + "*");
+  });
+  
 };
